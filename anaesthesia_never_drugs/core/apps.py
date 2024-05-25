d6b2e9b567ebddca8e23c14a5584aad8c2b527bf
Eamonn Upperton <uppertoe@gmail.com>
2024-05-25 10:56:49 +1000

    fix redis availability at build time

--- anaesthesia_never_drugs/core/apps.py
+++ anaesthesia_never_drugs/core/apps.py
@@ -14,9 +14,13 @@ class CoreAppConfig(AppConfig):
 
         # Check if CELERY_BROKER_URL is available before triggering caching
         celery_broker_url = os.getenv('CELERY_BROKER_URL')
-        
-        if celery_broker_url:
-            from .tasks import cache_common_queries
-            cache_common_queries.delay()
-        else:
+
+        if not celery_broker_url or celery_broker_url == "/":
             logger.info("CELERY_BROKER_URL is not set. Skipping Redis initialization.")
+        else:
+            try:
+                from .tasks import cache_common_queries
+                cache_common_queries.delay()
+                logger.info("Triggered cache_common_queries successfully.")
+            except Exception as e:
+                logger.error(f"Failed to trigger cache_common_queries: {e}")
